4aa2a47de28a2cfd1d5f6d2817a25a039653ed17
chanyu1 <haneul1ij@gmail.com>
2022-07-22 16:56:51 +0900

    learn TS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
-import _ from "lodash";
 
 new ProjectInput();
 new ProjectList("active");
